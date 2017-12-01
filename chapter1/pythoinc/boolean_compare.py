79317162ad79e960855e0d31ad46d4f84bb7248a
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-01 14:40:05 +0900

    Add chapter1/pythonic

---
+++ chapter1/pythoinc/boolean_compare.py
@@ -0,0 +1,15 @@
+###################################
+# File Name : bloolean_compare.py
+###################################
+#!/usr/bin/python3
+
+greeting = True
+
+if greeting:
+    print ("no operation")
+
+if greeting == True:
+    print ("'==' operation")
+
+if greeting is True:
+    print ("'is' operaion")
