38e707561780118187c1e632247cfecb6076f448
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-09 12:45:11 +0900

    Add chapter1/variable_shadowing

---
+++ chapter1/variable/variable_shadowing.py
@@ -0,0 +1,20 @@
+###################################
+# File Name : variable_shadowing.py
+###################################
+#!/usr/bin/python3
+
+var_shadowing = "global"
+
+def outer_function():
+    var_shadowing = "outer"
+
+    def inner_function():
+        var_shadowing = "inner"
+        print ("inner_function scope: %s" % var_shadowing)
+
+    inner_function()
+    print ("outer_function scope: %s" % var_shadowing)
+
+
+outer_function()
+print ("global scope: %s" % var_shadowing)
