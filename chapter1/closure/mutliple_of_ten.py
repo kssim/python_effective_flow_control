929491c44f0392be41dad9eea69d5af31350205a
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-10 15:57:35 +0900

    Add chapter1/closures

---
+++ chapter1/closure/mutliple_of_ten.py
@@ -0,0 +1,19 @@
+###################################
+# File Name : multiple_of_ten.py
+###################################
+#!/usr/bin/python3
+
+def multiple_of_ten():
+    square_root = 10
+
+    def square(x):
+        return square_root ** x
+
+    return square
+
+
+print ("=== print result ===")
+f = multiple_of_ten()
+
+print (f(2))
+print (f(3))
