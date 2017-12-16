c5221ab4ca89e7b368f06fa2e642c359ff0c6b71
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-16 23:13:07 +0900

    add closure misuse case.

---
+++ chapter1/closure/closure_misuse.py
@@ -0,0 +1,23 @@
+###################################
+# File Name : class_instead.py
+###################################
+#!/usr/bin/python3
+
+import datetime
+import time
+
+def logger():
+    now = datetime.datetime.now()
+
+    def print_log(msg):
+        return ("[%s] %s" % (now, msg))
+
+    return print_log
+
+
+log = logger()
+print (log("Start."))
+
+time.sleep(10)
+
+print (log("After 10 sec."))
