e733c0075ed34f0eae67e91ea6e0389362ab8641
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-21 16:03:44 +0900

    chapter2/exception

---
+++ chapter2/exception/exception.py
@@ -0,0 +1,25 @@
+###################################
+# File Name : exception.py
+###################################
+#!/usr/bin/python3
+
+EXIST_FILE="sample_file"
+NON_EXIST_FILE="non_exist_sample_file"
+
+def read_file(file_name):
+    try:
+        f = open(file_name, "r")
+    except:
+        print ("File open error")
+    else:
+        print (f.read())
+        f.close()
+    finally:
+        print ("End file read\n\n")
+
+
+print ("=== Exist file open ===")
+read_file(EXIST_FILE)
+
+print ("=== Non-Exists file open ===")
+read_file(NON_EXIST_FILE)
