0f70d943585d342de19f5d6e5058ba9b3e3f4e64
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-21 18:55:28 +0900

    chapter2/repetition

---
+++ chapter2/repetition/enumerate.py
@@ -0,0 +1,23 @@
+###################################
+# File Name : enumerate.py
+###################################
+#!/usr/bin/python3
+
+ALPHABET_LIST = ["a", "b", "c", "d", "e", "f"]
+
+def get_index_basic_method():
+    i = 0
+    for ch in ALPHABET_LIST:
+        print ("%d : %s" % (i, ch))
+        i += 1
+
+def get_index_enumerate_method():
+    for i, ch in enumerate(ALPHABET_LIST):
+        print ("%d : %s" % (i, ch))
+
+
+print ("=== Basic method ===")
+get_index_basic_method()
+
+print ("=== Enumerate method ===")
+get_index_enumerate_method()
