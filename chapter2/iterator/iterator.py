c97640d08d4c9837aca396351f72f685a1d199f1
KyeongSeob Sim <ksub0912@gmail.com>
2017-12-29 11:37:49 +0900

    chapter2/iterator

---
+++ chapter2/iterator/iterator.py
@@ -0,0 +1,13 @@
+###################################
+# File Name : iterator.py
+###################################
+#!/usr/bin/python3
+
+x = [1, 2, 3]
+x_iterator = iter(x)
+
+print ("=== print iterator ===")
+print (next(x_iterator))
+print (next(x_iterator))
+print (next(x_iterator))
+print (next(x_iterator))
