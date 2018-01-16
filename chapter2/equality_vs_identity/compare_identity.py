0674bf696c75f7885d8ecd8f05e127278a290039
KyeongSeob Sim <ksub0912@gmail.com>
2018-01-16 21:15:36 +0900

    Add chapter2/equality_vs_identity

---
+++ chapter2/equality_vs_identity/compare_identity.py
@@ -0,0 +1,11 @@
+###################################
+# File Name : compare_identity.py
+###################################
+#!/usr/bin/python3
+
+print ("=== compare identity ===")
+print (999 is 999)
+x = 999; y = 999;
+print (x is y)
+z = 999;
+print (x is z)
---
+++ chapter2/equality_vs_identity/compare_identity_analysis.py
@@ -0,0 +1,15 @@
+###################################
+# File Name : compare_identity_analysis.py
+###################################
+#!/usr/bin/python3
+
+print ("=== compare identity ===")
+print (999 is 999)
+x = 999; y = 999;
+print (x is y)
+z = 999;
+print (x is z)
+
+print (id(x))
+print (id(y))
+print (id(z))
