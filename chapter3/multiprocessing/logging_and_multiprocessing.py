ef3159d2dfb1cec69ec8a8a539fa79fbd7fc65e2
KyeongSeob Sim <ksub0912@gmail.com>
2018-02-04 16:53:05 +0900

    Add chapter3/process logging

---
+++ chapter3/multiprocessing/logging_and_multiprocessing.py
@@ -0,0 +1,20 @@
+###################################
+# File Name : logging_and_multiprocessing.py
+###################################
+#!/usr/bin/python3
+
+import logging
+import multiprocessing
+
+def worker(count):
+    print ("count : %s" % count)
+
+
+if __name__ == "__main__":
+    multiprocessing.log_to_stderr()
+    logger = multiprocessing.get_logger()
+    logger.setLevel(logging.DEBUG)
+
+    for i in range(5):
+        t = multiprocessing.Process(target=worker, args=(i,))
+        t.start()
