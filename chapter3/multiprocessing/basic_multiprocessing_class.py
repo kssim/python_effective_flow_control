f6f649106df34c7550927ac7434bd6eb170d625a
KyeongSeob Sim <ksub0912@gmail.com>
2018-02-04 13:06:49 +0900

    Add chapter3/multiprocessing basic

---
+++ chapter3/multiprocessing/basic_multiprocessing_class.py
@@ -0,0 +1,24 @@
+###################################
+# File Name : basic_multiprocessing_class.py
+###################################
+#!/usr/bin/python3
+
+import os
+import multiprocessing
+
+class Worker(multiprocessing.Process):
+
+    def __init__(self, name, args):
+        multiprocessing.Process.__init__(self)
+        self.name = name
+        self.args = args
+
+    def run(self):
+        print ("name : %s, argument : %s" % (self.name, self.args[0]))
+        print ("parent pid : %s, pid : %s" % (os.getppid(), os.getpid()))
+        print ("")
+
+
+for i in range(5):
+    p = Worker(name="process %i" % i, args=(i,))
+    p.start()
---
+++ chapter3/multiprocessing/basic_multiprocessing_function.py
@@ -0,0 +1,17 @@
+###################################
+# File Name : basic_multiprocessing_function.py
+###################################
+#!/usr/bin/python3
+
+import os
+import multiprocessing
+
+def worker(count):
+    print ("name : %s, argument : %s" % (multiprocessing.current_process().name, count))
+    print ("parent pid : %s, pid : %s" % (os.getppid(), os.getpid()))
+    print ("")
+
+
+for i in range(5):
+    p = multiprocessing.Process(target=worker, name="process %i" % i, args=(i,))
+    p.start()
