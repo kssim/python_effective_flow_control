5715f3d8dfe92433808abd69a51fc736b3603ab1
KyeongSeob Sim <ksub0912@gmail.com>
2018-03-02 20:08:30 +0900

    Add chapter4/native

---
+++ chapter4/asyncio/native.py
@@ -0,0 +1,20 @@
+###################################
+# File Name : native.py (Python 3.4 or later)
+###################################
+#!/usr/bin/python3
+
+import asyncio
+import random
+import datetime
+
+async def print_time(idx):
+    sleep_time = random.randrange(1, 10)
+    await asyncio.sleep(sleep_time)
+    print ("[%s] Sleep time : %s, Complete time : %s" % (idx, sleep_time, datetime.datetime.now()))
+
+
+futures = [print_time(i) for i in range(10)]
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.wait(futures))
+loop.close()
