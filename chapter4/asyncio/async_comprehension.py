7323bbf50e1ad06f7d4954d69c549b6c59373f2c
KyeongSeob Sim <ksub0912@gmail.com>
2018-03-03 11:04:59 +0900

    Add chapter4/async comprehension

---
+++ chapter4/asyncio/async_comprehension.py
@@ -0,0 +1,32 @@
+###################################
+# File Name : async_comprehension.py (Python 3.6 or later)
+###################################
+#!/usr/bin/python3
+
+import asyncio
+
+async def delay_range(to, delay=1):
+    for i in range(to):
+        yield i
+        await asyncio.sleep(delay)
+
+async def run():
+    print ("Async Comprehension")
+    return [i async for i in delay_range(3)]
+
+async def run_multiple():
+    print ("Async Await Comprehension")
+    f1 = run
+    f2 = run
+    func_list = [f1, f2]
+
+    result = [await func() for func in func_list]
+    print (result)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(run_multiple())
+    finally:
+        loop.close()
